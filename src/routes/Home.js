a7752f791ba36f719a450196fe76e00ae46fa005
GEUNA <geuna0204@gmail.com>
2021-12-15 16:50:56 +0900

    [add] 사진 업로드 시 미리보기 및 삭제 기능 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -12,6 +12,7 @@ import Tweet from 'components/Tweet';
 function Home({ userObj }) {
   const [tweet, setTweet] = useState('');
   const [tweets, setTweets] = useState([]);
+  const [imgFile, setImgFile] = useState();
 
   useEffect(() => {
     const q = query(
@@ -48,17 +49,61 @@ function Home({ userObj }) {
     setTweet(value);
   };
 
+  // 이미지는 한 장만 업로드 가능
+  const onFileChange = (e) => {
+    const { files } = e.target;
+    // input으로 들어온 모든 파일 중 첫번째 이미지만 선택되도록 설정
+    const myImage = files[0];
+
+    // reader를 만들고 eventListener 추가
+    const reader = new FileReader();
+    // 파일 로딩이 종료되면 finishedEvent를 가져온다.
+    reader.onloadend = (finishedEvent) => {
+      const { result } = finishedEvent.currentTarget;
+      setImgFile(result);
+    };
+    reader.readAsDataURL(myImage);
+  };
+
+  const clearPhoto = () => {
+    setImgFile(null);
+  };
+
   return (
     <div>
       <h1>Home</h1>
       <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          value={tweet}
-          onChange={onChange}
-          placeholder="What's on your mind?"
-          maxLength={120}
-        />
+        <div className="mention-container">
+          <label htmlFor="upload-tweet">Write your Tweeet</label>
+          <input
+            type="text"
+            name="upload-tweet"
+            value={tweet}
+            onChange={onChange}
+            placeholder="What's on your mind?"
+            maxLength={120}
+          />
+        </div>
+        <div className="image-container">
+          <label htmlFor="upload-image">Upload Image</label>
+          <input
+            type="file"
+            name="upload-image"
+            accept="image/*"
+            onChange={onFileChange}
+          />
+          {imgFile && (
+            <div>
+              <img
+                src={imgFile}
+                width="50px"
+                height="auto"
+                alt="uploaded file"
+              />
+              <button onClick={clearPhoto}>Clear</button>
+            </div>
+          )}
+        </div>
         <input type="submit" value="tweet" />
       </form>
       <ul>
