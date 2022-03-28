07a1ff69390d75ed560f92d8746927cbcd6b25fb
GEUNA <geuna0204@gmail.com>
2022-03-28 18:22:03 +0900

    [feat] MyTweets 페이지 생성

---
+++ src/pages/MyTweets.js
@@ -0,0 +1,5 @@
+const MyTweets = () => {
+  return <h1>MyTweets</h1>;
+};
+
+export default MyTweets;
---
+++ src/routes/Router.js
@@ -0,0 +1,44 @@
+import { Route, Routes } from 'react-router-dom';
+import Auth from 'pages/Auth';
+import Home from 'pages/Home';
+import Navigation from 'containers/Navigation';
+import UnauthenticatedNav from 'containers/UnauthenticatedNav';
+import Profile from 'pages/Profile';
+import styled from 'styled-components';
+import MyTweets from 'pages/MyTweets';
+
+const AppRouter = ({ isLoggedIn, userObj, refreshUser }) => {
+  return (
+    <>
+      <NavContainer>
+        {isLoggedIn ? <Navigation userObj={userObj} /> : <UnauthenticatedNav />}
+      </NavContainer>
+      <Routes>
+        {isLoggedIn ? (
+          <>
+            <Route path="/" element={<Home userObj={userObj} />} />
+            <Route
+              path="/profile"
+              element={<Profile userObj={userObj} refreshUser={refreshUser} />}
+            />
+            <Route path="/MyTweets" element={<MyTweets />} />
+          </>
+        ) : (
+          <>
+            <Route path="/" element={<Auth />} />
+            <Route path="/MyTweets" element={<Auth />} />
+            <Route path="/profile" element={<Auth />} />
+          </>
+        )}
+      </Routes>
+    </>
+  );
+};
+
+const NavContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default AppRouter;
